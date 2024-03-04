b11e4e2d24eeda01ed810c624d5a00fcead7c1db
Rayduxx <moussa.ala@esprit.tn>
2024-03-04 16:19:44 +0100

    Update MyDataBase.java

--- src/main/java/tn/esprit/utils/MyDataBase.java
+++ src/main/java/tn/esprit/utils/MyDataBase.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class MyDataBase {
     private static MyDataBase instance ;
-    private final String URL ="jdbc:mysql://127.0.0.1:3306/pidevdesktop";
+    private final String URL ="jdbc:mysql://127.0.0.1:3306/waves";
     private final String USERNAME="root";
     private final String PASSWORD ="";
 
